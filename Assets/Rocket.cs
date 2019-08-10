09e3f055d081baa2a86a1740a15af0adf196dc4e
Patrick Thompson <thompsonpa2014@gmail.com>
2019-08-10 12:57:57 -0400

    Rocket is flying with controls and can thrust on to landing pads and land

---
+++ Assets/Rocket.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+
+    Rigidbody RocketRB;
+    AudioSource RocketAS;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RocketRB = GetComponent<Rigidbody>();
+        RocketAS = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Thrust();
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        RocketRB.freezeRotation = true; //take control of the rockets physics rotation
+        
+        float rotationSpeed = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {           
+            transform.Rotate(Vector3.forward * rotationSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationSpeed);
+        }
+
+        RocketRB.freezeRotation = false; //resume the physics control of the rockets rotation
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            RocketRB.AddRelativeForce(Vector3.up * mainThrust);
+            if (!RocketAS.isPlaying)
+            {
+                RocketAS.Play();
+            }
+        }
+        else
+        {
+            RocketAS.Stop();
+        }
+    }
+}
